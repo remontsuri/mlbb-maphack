4293263b5702d261ed70ffd3826030342991d69d
remontsuri <vladcraft4@gmail.com>
2026-04-21 09:38:25 +0300

    Fix il2cpp typedef names and keyWindow

--- mlbb_analysis/maphack_src/maphack.m
+++ mlbb_analysis/maphack_src/maphack.m
@@ -10,32 +10,31 @@
 #include <mach-o/dyld.h>
 #include <string.h>
 
-// il2cpp types
 typedef void* Il2CppDomain;
 typedef void* Il2CppAssembly;
 typedef void* Il2CppImage;
 typedef void* Il2CppClass;
 typedef void* FieldInfo;
 
-typedef Il2CppDomain*    (*il2cpp_domain_get_t)(void);
-typedef Il2CppAssembly** (*il2cpp_domain_get_assemblies_t)(Il2CppDomain* domain, size_t* size);
-typedef Il2CppImage*     (*il2cpp_assembly_get_image_t)(Il2CppAssembly* assembly);
-typedef const char*      (*il2cpp_image_get_name_t)(Il2CppImage* image);
-typedef Il2CppClass*     (*il2cpp_class_from_name_t)(Il2CppImage* image, const char* ns, const char* name);
-typedef FieldInfo*       (*il2cpp_class_get_field_from_name_t)(Il2CppClass* klass, const char* name);
-typedef size_t           (*il2cpp_field_get_offset_t)(FieldInfo* field);
-typedef void*            (*il2cpp_class_get_static_field_data_t)(Il2CppClass* klass);
-typedef void             (*il2cpp_class_init_t)(Il2CppClass* klass);
+typedef Il2CppDomain*    (*fn_domain_get_t)(void);
+typedef Il2CppAssembly** (*fn_domain_get_assemblies_t)(Il2CppDomain* domain, size_t* size);
+typedef Il2CppImage*     (*fn_assembly_get_image_t)(Il2CppAssembly* assembly);
+typedef const char*      (*fn_image_get_name_t)(Il2CppImage* image);
+typedef Il2CppClass*     (*fn_class_from_name_t)(Il2CppImage* image, const char* ns, const char* name);
+typedef FieldInfo*       (*fn_class_get_field_t)(Il2CppClass* klass, const char* name);
+typedef size_t           (*fn_field_get_offset_t)(FieldInfo* field);
+typedef void*            (*fn_class_get_static_data_t)(Il2CppClass* klass);
+typedef void             (*fn_class_init_t)(Il2CppClass* klass);
 
-static il2cpp_domain_get_t                  _domain_get;
-static il2cpp_domain_get_assemblies_t       _domain_get_assemblies;
-static il2cpp_assembly_get_image_t          _assembly_get_image;
-static il2cpp_image_get_name_t              _image_get_name;
-static il2cpp_class_from_name_t             _class_from_name;
-static il2cpp_class_get_field_from_name_t   _class_get_field;
-static il2cpp_field_get_offset_t            _field_get_offset;
-static il2cpp_class_get_static_field_data_t _class_get_static_data;
-static il2cpp_class_init_t                  _class_init;
+static fn_domain_get_t          g_domain_get;
+static fn_domain_get_assemblies_t g_domain_get_assemblies;
+static fn_assembly_get_image_t  g_assembly_get_image;
+static fn_image_get_name_t      g_image_get_name;
+static fn_class_from_name_t     g_class_from_name;
+static fn_class_get_field_t     g_class_get_field;
+static fn_field_get_offset_t    g_field_get_offset;
+static fn_class_get_static_data_t g_class_get_static_data;
+static fn_class_init_t          g_class_init;
 
 static size_t off_m_ShowPlayers     = 0;
 static size_t off_ShowEntity        = 0;
@@ -58,7 +57,6 @@ static void*  bm_static_data        = NULL;
 }
 
 - (void)tick {
-    // Apply map hack every tick
     if (bm_static_data) {
         void* bm = *(void**)bm_static_data;
         if (bm) {
@@ -95,42 +93,42 @@ static BOOL loadIl2Cpp(void) {
     if (!uf_path) return NO;
     void* h = dlopen(uf_path, RTLD_LAZY | RTLD_NOLOAD);
     if (!h) return NO;
-    #define L(fn, sym) _##fn = (il2cpp_##fn##_t)dlsym(h, sym); if (!_##fn) return NO;
-    L(domain_get,              "il2cpp_domain_get")
-    L(domain_get_assemblies,   "il2cpp_domain_get_assemblies")
-    L(assembly_get_image,      "il2cpp_assembly_get_image")
-    L(image_get_name,          "il2cpp_image_get_name")
-    L(class_from_name,         "il2cpp_class_from_name")
-    L(class_get_field,         "il2cpp_class_get_field_from_name")
-    L(field_get_offset,        "il2cpp_field_get_offset")
-    L(class_get_static_data,   "il2cpp_class_get_static_field_data")
-    L(class_init,              "il2cpp_class_init")
-    #undef L
-    return YES;
+
+    g_domain_get              = (fn_domain_get_t)dlsym(h, "il2cpp_domain_get");
+    g_domain_get_assemblies   = (fn_domain_get_assemblies_t)dlsym(h, "il2cpp_domain_get_assemblies");
+    g_assembly_get_image      = (fn_assembly_get_image_t)dlsym(h, "il2cpp_assembly_get_image");
+    g_image_get_name          = (fn_image_get_name_t)dlsym(h, "il2cpp_image_get_name");
+    g_class_from_name         = (fn_class_from_name_t)dlsym(h, "il2cpp_class_from_name");
+    g_class_get_field         = (fn_class_get_field_t)dlsym(h, "il2cpp_class_get_field_from_name");
+    g_field_get_offset        = (fn_field_get_offset_t)dlsym(h, "il2cpp_field_get_offset");
+    g_class_get_static_data   = (fn_class_get_static_data_t)dlsym(h, "il2cpp_class_get_static_field_data");
+    g_class_init              = (fn_class_init_t)dlsym(h, "il2cpp_class_init");
+
+    return (g_domain_get && g_class_from_name && g_class_get_field) ? YES : NO;
 }
 
 static void findOffsets(void) {
-    Il2CppDomain* domain = _domain_get();
+    Il2CppDomain* domain = g_domain_get();
     if (!domain) return;
     size_t count = 0;
-    Il2CppAssembly** assemblies = _domain_get_assemblies(domain, &count);
+    Il2CppAssembly** assemblies = g_domain_get_assemblies(domain, &count);
     if (!assemblies) return;
     Il2CppImage* img = NULL;
     for (size_t i = 0; i < count; i++) {
-        Il2CppImage* im = _assembly_get_image(assemblies[i]);
+        Il2CppImage* im = g_assembly_get_image(assemblies[i]);
         if (!im) continue;
-        const char* n = _image_get_name(im);
+        const char* n = g_image_get_name(im);
         if (n && strcmp(n, "Assembly-CSharp.dll") == 0) { img = im; break; }
     }
     if (!img) { NSLog(@"[MapHack] Assembly-CSharp not found"); return; }
-    Il2CppClass* bm = _class_from_name(img, "", "BattleManager");
+    Il2CppClass* bm = g_class_from_name(img, "", "BattleManager");
     if (!bm) { NSLog(@"[MapHack] BattleManager not found"); return; }
-    _class_init(bm);
-    bm_static_data = _class_get_static_data(bm);
+    if (g_class_init) g_class_init(bm);
+    if (g_class_get_static_data) bm_static_data = g_class_get_static_data(bm);
     FieldInfo* f;
-    if ((f = _class_get_field(bm, "m_ShowPlayers")))     off_m_ShowPlayers     = _field_get_offset(f);
-    if ((f = _class_get_field(bm, "ShowEntity")))        off_ShowEntity        = _field_get_offset(f);
-    if ((f = _class_get_field(bm, "m_LocalPlayerShow"))) off_m_LocalPlayerShow = _field_get_offset(f);
+    if ((f = g_class_get_field(bm, "m_ShowPlayers")))     off_m_ShowPlayers     = g_field_get_offset(f);
+    if ((f = g_class_get_field(bm, "ShowEntity")))        off_ShowEntity        = g_field_get_offset(f);
+    if ((f = g_class_get_field(bm, "m_LocalPlayerShow"))) off_m_LocalPlayerShow = g_field_get_offset(f);
     NSLog(@"[MapHack] BM=%p ShowPlayers=0x%zx ShowEntity=0x%zx Local=0x%zx",
           bm_static_data, off_m_ShowPlayers, off_ShowEntity, off_m_LocalPlayerShow);
 }
